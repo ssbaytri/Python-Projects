7bdb58d99d86876a687ccb5af6bf704ef7df97ca
SubGameR69e <azjoker333@gmail.com>
2024-04-15 21:24:11 +0100

    End of PIG Game

--- PIG Game/game.py
+++ PIG Game/game.py
@@ -4,22 +4,48 @@ def roll():
     min_value = 1
     max_value = 6
     roll = random.randint(min_value, max_value)
+
     return roll
 
 while True:
-    players = input("Enter the number of players (2 -> 4): ")
+    players = input("Enter the number of player (2 -> 4): ")
     if players.isdigit():
         players = int(players)
-        if players >= 2 and players <= 4:
+        if 2<= players <= 4:
             break
         else:
-            print("Please enter a number between 2 and 4.")
+            print("Must be between 2 - 4 players.")
     else:
-        print("Invalid Input, try again.")
+        print("Invalid, try again.")
 
 max_score = 50
-players_score = [0 for _ in range(players)]
+player_score = [0 for _ in range(players)]
 
-while max(players_score) < max_score:
-    for player_idx in range(players):
-        print("\nPlayer", + player_idx + 1 )
+while max(player_score) < max_score:
+    for player_index in range(players):
+        print("\nPlayer", player_index + 1, "turn has just started!")
+        print("Your total score is:", player_score[player_index], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+            
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! , Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value, )
+
+            print("Your current score is:", current_score)
+
+        player_score[player_index] += current_score
+        print("Your total score is:", player_score[player_index])
+
+max_score = max(player_score)
+winner_player_index = player_score.index(max_score)
+print("The Winner is the player:", winner_player_index + 1, "with a score of:", max_score)
