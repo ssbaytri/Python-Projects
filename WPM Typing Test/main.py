8b819009777fafb171c3fa6a4e608cd383a08fdc
SubGameR69e <azjoker333@gmail.com>
2024-05-12 21:42:31 +0100

    UX Functions

--- WPM Typing Test/main.py
+++ WPM Typing Test/main.py
@@ -1,10 +1,43 @@
 import curses
 from curses import wrapper
 
-def main(stdscr):
+def start_programm(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello World")
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to continue!")
     stdscr.refresh()
     stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!\n"
+    current_text = []
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(2))
+        
+        stdscr.refresh()
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        if key in ("KEY_BACKSPACE", "\b", "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_programm(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
