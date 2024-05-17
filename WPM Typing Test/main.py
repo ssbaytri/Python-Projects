d6a46b25ec3d2e32a61f78ba62d589c93b7e9988
SubGameR69e <azjoker333@gmail.com>
2024-05-17 23:44:37 +0100

    load_text def and end of project.

--- WPM Typing Test/main.py
+++ WPM Typing Test/main.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import time
+import random
 
 def start_programm(stdscr):
     stdscr.clear()
@@ -23,8 +24,13 @@ def display_text(stdscr, target, current, wpm=0):
         stdscr.addstr(0, i, char, color)
 
 
+def load_text():
+    with open("texts.txt", "r") as file:
+        lines = file.readlines()
+        return random.choice(lines).strip()
+
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = load_text()
     current_text = []
     wpm = 0
     start_time = time.time()
@@ -36,6 +42,10 @@ def wpm_test(stdscr):
         stdscr.clear()
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
+
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
 
         try:
             key = stdscr.getkey()
@@ -58,6 +68,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_programm(stdscr)
-    wpm_test(stdscr)
+
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text!, Press any key to continue")
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
 
 wrapper(main)
