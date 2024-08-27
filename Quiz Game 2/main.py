38b9f052c44cb9151f916806d18362c4cb8469af
SubGameR69e <azjoker333@gmail.com>
2024-08-27 22:51:30 +0100

    setup the file

---
+++ Quiz Game 2/main.py
@@ -0,0 +1,22 @@
+# Quiz Game (Advanced)
+
+questions = (
+    "What is the capital of France?",
+    "Who wrote the play 'Romeo and Juliet'?",
+    "What is the chemical symbol for water?",
+    "Which planet is known as the Red Planet?",
+    "What is the largest mammal in the world?"
+)
+
+options = (
+    ("A. Berlin", "B. London", "C. Paris", "D. Madrid"),
+    ("A. Charles Dickens", "B. William Shakespeare", "C. Jane Austen", "D. Mark Twain"),
+    ("A. N2", "B. CO2", "C. O2", "D. H2O"),
+    ("A. Saturn", "B. Venus", "C. Jupiter", "D. Mars"),
+    ("A. Blue Whale", "B. Elephant", "C. Giraffe", "D. Great White Shark")
+)
+
+answers = ("C", "B", "D", "D", "A")
+guesses = []
+score = 0
+question_number = 0
