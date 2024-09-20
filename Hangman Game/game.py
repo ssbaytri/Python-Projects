b0de4ab9c998c57a92eca522a0647f23b96dd0e4
SubGameR69e <azjoker333@gmail.com>
2024-09-20 01:14:33 +0100

    wordlist

--- Hangman Game/game.py
+++ Hangman Game/game.py
@@ -2,7 +2,7 @@
 
 import random
 
-words = ("computer", "space", "energy", "pickaxe", "science", "delivery")
+words = ("computer", "space", "energy", "pickaxe", "science", "delivery", "work", "gem")
 
 # Hangman stages dictionary
 hangman_stages = {
@@ -73,19 +73,57 @@ hangman_stages = {
 
 
 def display_man(wrong_guesses):
-    pass
+    print(hangman_stages[wrong_guesses])
 
 
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
 
 
 def main():
-    pass
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    running = True
+
+    while running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid Input!")
+            continue
+
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed!")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        else:
+            wrong_guesses += 1
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("You Won!")
+            running = False
+        elif wrong_guesses >= len(hangman_stages) - 1:
+            display_man(wrong_guesses)
+            print("You Lost!, The answer was: ")
+            display_answer(answer)
+            running = False
+
 
 
 if __name__ == "__main__":
---
+++ Hangman Game/wordlist.py
@@ -0,0 +1,14 @@
+# wordlist for hangman game
+word_list = [
+    "python", "javascript", "hangman", "developer", "keyboard",
+    "programming", "algorithm", "function", "variable", "syntax",
+    "iteration", "exception", "computer", "debugging", "binary",
+    "compile", "execute", "hardware", "software", "internet",
+    "protocol", "database", "operator", "recursion", "encryption",
+    "array", "boolean", "framework", "inheritance", "object",
+    "module", "package", "stack", "queue", "pointer", "dictionary",
+    "class", "tuple", "loop", "string", "condition", "syntax",
+    "virtual", "memory", "constructor", "destructor", "method",
+    "interface", "network", "server", "client", "protocol",
+    "variable", "integer", "float", "exception", "library"
+]
