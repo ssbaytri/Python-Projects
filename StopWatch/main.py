f2c65e6421fbf1369ea599184efdec07cdae002f
SubGameR69e <azjoker333@gmail.com>
2024-09-21 20:58:27 +0100

    app setup

--- StopWatch/main.py
+++ StopWatch/main.py
@@ -1,1 +1,16 @@
 # Python StopWatch with PyQt5
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import QTime, QTimer, Qt
+
+
+class StopWatch(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    stop_watch = StopWatch()
+    stop_watch.show()
+    sys.exit(app.exec_())
