30ada1456b38ab2d95336ecc1a40546dde95fcc1
SubGameR69e <azjoker333@gmail.com>
2024-09-28 20:17:24 +0100

    draw the maze

--- Shortest Path Finder/main.py
+++ Shortest Path Finder/main.py
@@ -1,5 +1,10 @@
+import curses
+from curses import wrapper
+import queue
+import time
+
 maze = [
-    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#", "#", "O", "#", "#"],
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
     ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
@@ -9,3 +14,25 @@ maze = [
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
+
+
+def draw_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, col in enumerate(row):
+            stdscr.addstr(i, j*2, col, BLUE)
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    stdscr.clear()
+    draw_maze(maze, stdscr)
+    stdscr.refresh()
+    stdscr.getch()
+
+
+wrapper(main)
