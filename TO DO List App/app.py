6fefb126826aaf8e8b7f29fe771fad547d34ce49
ssbaytri <azjoker333@gmail.com>
2025-08-30 20:47:45 +0100

    db conn & display todos

--- TO DO List App/app.py
+++ TO DO List App/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class TODO(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    completed = db.Column(db.Boolean)
 
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    todo_list = TODO.query.all()
+    return render_template("base.html", todos=todo_list)
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
