230c11ff4f6e6004c6a23d2598480c0abb040779
ssbaytri <azjoker333@gmail.com>
2026-02-05 12:21:22 +0100

    adding frontend

--- FastAPI Project/app/app.py
+++ FastAPI Project/app/app.py
@@ -23,7 +23,7 @@ app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jw
 app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_reset_password_router(), prefix="/auth", tags=["auth"])
 app.include_router(fastapi_users.get_verify_router(UserRead), prefix="/auth", tags=["auth"])
-app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
 
 @app.post("/upload")
 async def upload_file(
